fb90ca9e235b28364044db0dff4c00032cb6ba91
kunalsharma1997 <58587883+kunalsharma1997@users.noreply.github.com>
2019-12-06 21:29:19 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -5,11 +5,11 @@ int main()
 {
 int a=3;
 int b=4;
-printf("original value of a=%d", a);
-printf("original value of b=%d", b);
+printf("original value of a=%d\n", a);
+printf("original value of b=%d\n", b);
 swap(&a,&b);
-printf("after swaping value of a=%d", a);
-printf("after swaping value of b=%d", b);
+printf("after swaping value of a=%d\n", a);
+printf("after swaping value of b=%d\n", b);
 return 0;
 }
 void swap(int *x,int *y)
